a3510e4d46b27e6949fe164d7847db671e43ecfe
shashi-kant10 <shashiofficial10@gmail.com>
2022-10-15 11:51:08 +0530

    Added Model class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Date;
+
+class Student {
+    String name;
+    int roll;
+    String branch;
+    Date date;
+    boolean isPresent;
+
+    Student(String n, int r, String b, boolean p) {
+        name = n;
+        roll = r;
+        branch = b;
+        date = new Date();
+        isPresent = p;
+    }
+
+}
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+
     }
 }
