5a3da5575f6e025489572a364e24b19c61304b7d
shashi-kant10 <shashiofficial10@gmail.com>
2022-10-15 12:46:17 +0530

    Added AttendanceManagement class

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,75 @@
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Date;
+import java.util.Scanner;
 
 class Student {
     String name;
     int roll;
-    String branch;
     Date date;
     boolean isPresent;
 
-    Student(String n, int r, String b, boolean p) {
+    Student(String n, int r) {
         name = n;
         roll = r;
-        branch = b;
-        date = new Date();
-        isPresent = p;
+        isPresent = false;
     }
 
 }
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+class AttendanceManagement {
 
+    ArrayList<Student> studentArrayList = new ArrayList<>();
+
+    void putStudentData(Student student) {
+        studentArrayList.add(student);
+        sort(studentArrayList);
+    }
+
+    void takeAttendance() {
+        if (isEmpty()) {
+            System.out.println("No data available");
+            return;
+        }
+
+        System.out.println("Attendance on " + (new Date()));
+
+        for (Student s : studentArrayList) {
+            s.date = new Date();
+
+            System.out.println("Is roll " + s.roll + " present? y:yes,n:no");
+
+            Scanner sc = new Scanner(System.in);
+            char ch = sc.next().charAt(0);
+
+            if (ch == 'y') {
+                s.isPresent = true;
+            } else
+                s.isPresent = false;
+        }
+    }
+
+    void viewAttendance() {
+        if (isEmpty()) {
+            System.out.println("No data available");
+            return;
+        }
+
+        for (Student s : studentArrayList) {
+            System.out.println("Name: " + s.name);
+            System.out.println("Roll: " + s.roll);
+            System.out.println("Date: " + s.date.toString());
+            System.out.println("Present: " + ((s.isPresent == true) ? "Yes" : "NO"));
+        }
+    }
+
+    boolean isEmpty() {
+        return studentArrayList.size() == 0 ? true : false;
+    }
+
+    public static void sort(ArrayList<Student> list) {
+
+        list.sort(Comparator.comparing(o -> o.date));
 
     }
 }
